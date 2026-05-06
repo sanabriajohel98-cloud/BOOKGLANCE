c53a39ef049403517e28e9421bd80dd9e0318658
sanabriajohel98-cloud <sanabriajohel98@gmail.com>
2026-05-06 00:25:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -69,12 +69,7 @@ def debug():
     except Exception as e:
         return f"❌ Error: {str(e)}"
 
-# Crear DB solo una vez, manualmente
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    print("Base de datos inicializada.")
-    app.run(debug=True)
+
    
 
 # 🔐 LOGIN
