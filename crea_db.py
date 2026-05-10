c57c7b9288c4be4d2c5bb60a2c45fe19afdd6bdd
Johel <sanabriajohe98@gmail.com>
2026-05-09 22:45:07 -0300

    Bookgs

--- crea_db.py
+++
@@ -1,6 +0,0 @@
-# Crear DB solo una vez, manualmente
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    print("Base de datos inicializada.")
-    app.run(debug=True)
