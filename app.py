cbbf6950e2be713ec33f1c90e688e41728f0e535
Johel <sanabriajohe98@gmail.com>
2026-05-15 00:39:31 -0300

    Bookgs

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ from app import app, db
 from flask_migrate import upgrade
 
 with app.app_context():
-    upgrade()  # aplica las migraciones pendientes
+    flask_migrate.upgrade()  # aplica las migraciones pendientes
     print("✅ Migraciones aplicadas correctamente, productos e imágenes conservados.")
 
 
