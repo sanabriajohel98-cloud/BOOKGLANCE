a87fd57cdbc72de3641c146173fd2688f8aef9be
Johel <sanabriajohe98@gmail.com>
2026-05-15 02:00:37 -0300

    Bookgs

--- app.py
+++ app.py
@@ -77,13 +77,6 @@ def debug():
 
    # createDB.py
 
-# createDB.py
-from app import app, db
-from flask_migrate import upgrade
-
-with app.app_context():
-      upgrade()  # aplica las migraciones pendientes
-      print("✅ Migraciones aplicadas correctamente, productos e imágenes conservados.")
 
 # 🔐 LOGIN
 @app.route("/", methods=["GET", "POST"])
--- createDB.py/createDB.py
+++
@@ -1,7 +0,0 @@
-# createDB.py
-from app import app, db
-from flask_migrate import upgrade
-
-with app.app_context():
-      upgrade()  # aplica las migraciones pendientes
-      print("✅ Migraciones aplicadas correctamente, productos e imágenes conservados.")
